930b28f88fcc789a6f185131ff02fec6bb6a0077
Doris Tang <huiyao.tang@gmail.com>
2015-06-17 16:09:21 -0400

    * Added initial project files

---
+++ src/wormguides/MainApp.java
@@ -0,0 +1,49 @@
+package wormguides;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+
+	public MainApp() {
+		
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("WormGUIDES");
+		
+		initRootLayout();
+	}
+	
+	public void initRootLayout() {
+		try {
+            // Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/MainApp.fxml"));
+            rootLayout = (AnchorPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+            
+        } catch (IOException e) {
+        	System.out.println("Could not initialize root layout.");
+            e.printStackTrace();
+        }
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
