f283de351edf4d301c93fdaeac658680a5e1fb33
Doris Tang <huiyao.tang@gmail.com>
2015-06-19 15:22:45 -0400

    * Set up main application basic views

--- src/wormguides/MainApp.java
+++ src/wormguides/MainApp.java
@@ -6,15 +6,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 	
 	private Stage primaryStage;
-	private AnchorPane rootLayout;
+	private BorderPane rootLayout;
+	private BorderPane displayPanel;
 
 	public MainApp() {
-		
 	}
 	
 	@Override
@@ -23,19 +24,22 @@ public class MainApp extends Application {
 		this.primaryStage.setTitle("WormGUIDES");
 		
 		initRootLayout();
+		init3DWindow();
 	}
 	
 	public void initRootLayout() {
 		try {
-            // Load root layout from fxml file.
+            // Load root layout from FXML file.
             FXMLLoader loader = new FXMLLoader();
-            loader.setLocation(MainApp.class.getResource("view/MainApp.fxml"));
-            rootLayout = (AnchorPane) loader.load();
-
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+            
             // Show the scene containing the root layout.
             Scene scene = new Scene(rootLayout);
             primaryStage.setScene(scene);
-            primaryStage.setResizable(false);
+            
+            // Get reference to right-side panel to add 3D Window later
+            displayPanel = (BorderPane) scene.lookup("#displayPanel");
             
             primaryStage.show();
             
@@ -44,11 +48,26 @@ public class MainApp extends Application {
             e.printStackTrace();
         }
 	}
-
+	
+	public void init3DWindow() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/Window3DContainer.fxml"));
+			AnchorPane container3D = (AnchorPane) loader.load();
+			displayPanel.setCenter(container3D);
+			
+		} catch (IOException e) {
+			System.out.println("Could not initialize 3D Window.");
+            e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
+		
 		AceTreeLoader.loadNucFiles(JAR_NAME);
 	}
 	
 	private static final String JAR_NAME = "WormGUIDES.jar";
+	private static final double ASPECT_RATIO = (3/4);
 }
---
+++ src/wormguides/view/Window3DContainerController.java
@@ -0,0 +1,18 @@
+package wormguides.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+
+public class Window3DContainerController {
+	
+	@FXML
+	private Canvas canvas3D;
+	
+	@FXML
+	private AnchorPane window3DContainer;
+	
+	@FXML
+	private Slider timeSlider;
+}
