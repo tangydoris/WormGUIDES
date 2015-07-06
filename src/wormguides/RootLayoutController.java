7a661ed89bb65d475c0c50a46f36678cc12c022c
Doris Tang <huiyao.tang@nyu.edu>
2015-07-06 10:29:31 -0400

    * Made about page popup window

--- src/wormguides/RootLayoutController.java
+++ src/wormguides/RootLayoutController.java
@@ -1,8 +1,23 @@
 package wormguides;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class RootLayoutController {
+	
+	//@FXML private MenuItem menuAbout;
+	@FXML
+	private TextArea aboutText;
+	
+	private Stage aboutStage;
+	private Parent aboutRoot;
 	
 	@FXML
 	protected void menuCloseAction() {
@@ -12,7 +27,22 @@ public class RootLayoutController {
 	
 	@FXML
 	protected void menuAboutAction() {
-		System.out.println("about");
+		if (aboutStage == null) {
+			aboutStage = new Stage();
+			try {
+				aboutRoot = FXMLLoader.load(getClass().getResource("view/About.fxml"));
+				aboutStage.setScene(new Scene(aboutRoot));
+				aboutStage.setTitle("About WormGUIDES");
+				aboutStage.initModality(Modality.APPLICATION_MODAL);
+				aboutStage.show();
+			} catch (IOException e) {
+				System.out.println("cannot load about page.");
+				e.printStackTrace();
+			}
+		}
+		else
+			aboutStage.show();
+		
 	}
 	
 }
