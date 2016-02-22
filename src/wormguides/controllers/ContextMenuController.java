eb94e5066fbf1b3b0ab05ec3dd883e2afdd44371
Doris Tang <huiyao.tang@nyu.edu>
2016-02-21 23:47:54 -0500

    * Fixed expressions/wired to graying out

--- src/wormguides/controllers/ContextMenuController.java
+++ src/wormguides/controllers/ContextMenuController.java
@@ -228,7 +228,7 @@ public class ContextMenuController extends AnchorPane implements Initializable {
 	}
 	
 	public void disableTerminalCaseFunctions(boolean disable) {
-		expresses.setDisable(disable);
+		wiredTo.setDisable(disable);
 	}
 
 	@Override
